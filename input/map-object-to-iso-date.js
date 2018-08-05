de5cc1f7fdc1d04473b3674a38a2ce476de93fd7
bergi <bergi@axolotlfarm.org>
2018-08-06 00:04:35 +0200

    map alternative date format

--- input/map-object-to-iso-date.js
+++ input/map-object-to-iso-date.js
@@ -1,6 +1,7 @@
 const moment = require('moment')
 const p = require('barnard59')
 
+const ddmmyyyyhhmmRegExp = new RegExp('\\d{2}[.]\\d{2}[.]\\d{4} \\d{2}:\\d{2}')
 const ddmmyyyyhhmmssRegExp = new RegExp('\\d{2}[.]\\d{2}[.]\\d{4} \\d{2}:\\d{2}:\\d{2}')
 
 function mapObjectToIsoDate (quad) {
@@ -13,6 +14,12 @@ function mapObjectToIsoDate (quad) {
   }
 
   let object = quad.object
+
+  if (ddmmyyyyhhmmRegExp.test(quad.object.value)) {
+    const date = moment(quad.object.value, 'DD.MM.YYYY HH:mm').toISOString()
+
+    object = p.rdf.literal(date, quad.object.datatype)
+  }
 
   if (ddmmyyyyhhmmssRegExp.test(quad.object.value)) {
     const date = moment(quad.object.value, 'DD.MM.YYYY HH:mm:ss').toISOString()
--- input/map-subject-to-iso-date.js
+++ input/map-subject-to-iso-date.js
@@ -1,6 +1,7 @@
 const moment = require('moment')
 const p = require('barnard59')
 
+const ddmmyyyyhhmmRegExp = new RegExp('\\d{2}[.]\\d{2}[.]\\d{4} \\d{2}:\\d{2}')
 const ddmmyyyyhhmmssRegExp = new RegExp('\\d{2}[.]\\d{2}[.]\\d{4} \\d{2}:\\d{2}:\\d{2}')
 
 function mapSubjectToIsoDate (quad) {
@@ -11,6 +12,13 @@ function mapSubjectToIsoDate (quad) {
   let subject = quad.subject
 
   const pathEnd = decodeURIComponent(subject.value).split('/').pop()
+
+  if (ddmmyyyyhhmmRegExp.test(pathEnd)) {
+    const pathStart = subject.value.split('/').slice(0, -1).join('/')
+    const date = encodeURIComponent((moment(pathEnd, 'DD.MM.YYYY HH:mm')).toISOString().split(':').join(''))
+
+    subject = p.rdf.namedNode(`${pathStart}/${date}`)
+  }
 
   if (ddmmyyyyhhmmssRegExp.test(pathEnd)) {
     const pathStart = subject.value.split('/').slice(0, -1).join('/')
